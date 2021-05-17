4689a84fcb1f35d6555d15f28c346a9534f7ec32
user <user@users-MacBook-Pro.local>
2021-05-17 17:32:58 +0530

    Initial Commit

---
+++ NotificationLib/src/main/java/com/pushnotification/notificationlib/Notification.java
@@ -0,0 +1,57 @@
+package com.pushnotification.notificationlib;
+
+import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class Notification extends FirebaseMessagingService {
+    @Override
+    public void
+    onMessageReceived(RemoteMessage remoteMessage) {
+        Log.wtf("message", String.valueOf(remoteMessage.getData()));
+        if (remoteMessage.getNotification() != null) {
+            showNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+        }
+    }
+
+
+    public void showNotification(String title, String message) {
+        // Intent intent = new Intent(this, activity.getClass());
+        String channel_id = "notification_channel";
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), channel_id)
+                .setAutoCancel(true)
+                .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
+                .setOnlyAlertOnce(true);
+//                .setContentIntent(pendingIntent);
+        builder = builder.setContentTitle(title).setContentText(message);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(channel_id, "Notification Channel", NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+        notificationManager.notify(0, builder.build());
+    }
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        Log.wtf("token", s);
+        super.onNewToken(s);
+    }
+}
