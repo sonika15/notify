1c60cf351f958d48b707523f24e9c149df790fd4
user <sonikakolhe@gmail.com>
2021-05-17 20:18:20 +0530

    Initial Commit

---
+++ NotificationLib/build/generated/source/buildConfig/debug/com/pushnotification/notificationlib/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.pushnotification.notificationlib;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.pushnotification.notificationlib";
+  public static final String BUILD_TYPE = "debug";
+}
